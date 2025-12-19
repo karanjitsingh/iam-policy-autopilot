4622644498cd3944a2d849487f7e38d07dcfa9c5
Karan Jit Singh <karan1866@gmail.com>
2025-12-19 10:01:53 -0800

    fix: update service reference user agent for integration tests (#86)

--- iam-policy-autopilot-policy-generation/src/enrichment/service_reference.rs
+++ iam-policy-autopilot-policy-generation/src/enrichment/service_reference.rs
@@ -385,8 +385,18 @@ impl RemoteServiceReferenceLoader {
     }
 
     fn create_client() -> crate::errors::Result<Client> {
-        let user_agent = format!("{}/{}", IAM_POLICY_AUTOPILOT, env!("CARGO_PKG_VERSION"));
+        let user_agent_suffix = if cfg!(feature = "integ-test") {
+            "-integration-test"
+        } else {
+            ""
+        };
 
+        let user_agent = format!(
+            "{}{}/{}",
+            IAM_POLICY_AUTOPILOT,
+            user_agent_suffix,
+            env!("CARGO_PKG_VERSION")
+        );
         Client::builder()
             .user_agent(user_agent)
             .build()
